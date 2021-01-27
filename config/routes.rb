b16d57c854ef0e26094c8344bd24afaa0980138d
+ <mcrea23@aol.com>
2021-01-27 14:53:37 -0600

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+resources :hikers 
+resources :trails 
+resources :parks 
+resources :reviews 
